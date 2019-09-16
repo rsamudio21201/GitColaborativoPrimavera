ebe39e253ea8c2857fc56e48f5c1638e5128d582
luisviza <viza.boss13@gmail.com>
2019-09-15 23:39:22 -0400

    Insertar la Clase Vehiculos

---
+++ PruebaGitColaborativo/Vehiculos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGitColaborativo
+{
+    public class Vehiculos
+    {
+        public string VehiculoId { get; set; }
+        public string Matricula { get; set; }
+        public string Modelo { get; set; }
+
+
+    }
+}
